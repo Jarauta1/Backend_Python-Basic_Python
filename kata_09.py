fe17758980f1f166ed64809017e271344c23f09a
Jarauta1 <diegojarauta8@gmail.com>
2021-05-11 23:40:10 +0200

    ejercicio 10

--- kata_09.py
+++
@@ -1,7 +0,0 @@
-money = float(input("¿Qué cantidad desea invertir?: "))
-percent = float(input("¿Qué interés porcentual anual quiere?: "))
-years = float(input("¿Cuántos años desea invertir?: "))
-
-benefits = round(money * (percent/100+1) ** years,2)
-print("Usted invirtió " + str(round(money,2)) + "€.")
-print("Ahora tiene " + str(round(benefits,2)) + "€.")
