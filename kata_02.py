02bbdaef16afa7b0d93a5beb9be73d6a3bc3ac75
Jarauta1 <diegojarauta8@gmail.com>
2021-05-11 18:39:48 +0200

    ejercicio 3

--- kata_02.py
+++
@@ -1,9 +0,0 @@
-def compra(cantidad):
-    if cantidad > 100:
-        cuenta = cantidad - ((cantidad*10)/100)
-    else:
-        cuenta = cantidad
-    print("La cuenta es: " + str(cuenta) + "€")
-
-if __name__ == '__main__':
-    compra(1000)
---
+++ kata_03.py
@@ -0,0 +1,7 @@
+def anyo(dato):
+    while dato <= 2055:
+        print("Informe del Año " + str(dato))
+        dato += 1
+
+if __name__ == '__main__':
+    anyo(2046)
