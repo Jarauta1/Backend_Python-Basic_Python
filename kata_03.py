f96e2ee4ebb4b2bb8b5f5f05262694b9a3e078ff
Jarauta1 <diegojarauta8@gmail.com>
2021-05-11 18:59:17 +0200

    ejercicio 4

--- kata_03.py
+++
@@ -1,7 +0,0 @@
-def anyo(dato):
-    while dato <= 2055:
-        print("Informe del Año " + str(dato))
-        dato += 1
-
-if __name__ == '__main__':
-    anyo(2046)
---
+++ kata_04.py
@@ -0,0 +1,7 @@
+mi_lista = ["Diego", "Laura", "Pilar", "José", "Luis"]
+
+tope = len(mi_lista)
+indice = 0
+while indice < tope:
+    print(mi_lista[indice])
+    indice += 1
