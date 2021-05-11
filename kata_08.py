f7b43935865f446f875477d770f7a4e81a824f0f
Jarauta1 <diegojarauta8@gmail.com>
2021-05-11 23:14:22 +0200

    ejercicio 9

--- kata_08.py
+++
@@ -1,6 +0,0 @@
-peso = input("¿Cuánto pesas?(en kg): ")
-altura = input("¿Cuánto mides?(en metros): ")
-
-imc = round((float(peso)/(float(altura)**2)),2)
-
-print("Tu indice de masa corporal es: " + str(imc))
