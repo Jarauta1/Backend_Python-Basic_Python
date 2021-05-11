219aab003a2eaa8e3720f9e2137a83a1e164912b
Jarauta1 <diegojarauta8@gmail.com>
2021-05-11 19:19:31 +0200

    corregido ejercicio 6

--- kata_06.py
+++ kata_06.py
@@ -1,9 +1,9 @@
-def impar(tope):
-    num = 1
-    while num < tope:
+def impar(inicio,fin):
+    num = inicio
+    while num < fin:
         resto = num % 2
         if resto != 0:
             print(num)
         num += 1
 
-impar(10)
+impar(4,10)
