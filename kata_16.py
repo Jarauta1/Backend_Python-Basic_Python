1bb48c79518feaf1c2d27c2866f325e39a76ac75
Jarauta1 <diegojarauta8@gmail.com>
2021-05-12 09:35:10 +0200

    ejercicio 16

---
+++ kata_16.py
@@ -0,0 +1,8 @@
+num1 = int(input("Numero 1: "))
+num2 = int(input("Numero 2: "))
+num3 = int(input("Numero 3: "))
+
+def promedio(a,b,c):
+  return (a+b+c)/3
+
+print("El promedio es: "+ str(promedio(num1,num2,num3)))
