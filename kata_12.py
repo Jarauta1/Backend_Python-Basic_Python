9b5be3955cae38efe286bdd0b6ca9d1ba4d4908b
Jarauta1 <diegojarauta8@gmail.com>
2021-05-12 07:56:09 +0200

    ejercicio 12

---
+++ kata_12.py
@@ -0,0 +1,8 @@
+def intermedio(a,b):
+  return (a+b)/2
+ 
+a = int(input("Número 1: "))
+b = int(input("Número 2: "))
+solucion = intermedio(a,b)
+
+print("El valor medio de los dos numeros es: " + str(solucion))
